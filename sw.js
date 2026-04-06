3384261ba5701a87b9db101ea2c18e75d002aa45
zacpaulson <zac.paulson@gmail.com>
2026-04-05 21:21:00 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+var CACHE = 'fc-cache-v1';
+var SHELL = ['./'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) { return c.addAll(SHELL); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  var url = e.request.url;
+  // Never intercept Apps Script API calls
+  if (url.indexOf('script.google.com') !== -1) { return; }
+  // Cache Google Fonts
+  if (url.indexOf('fonts.googleapis.com') !== -1 || url.indexOf('fonts.gstatic.com') !== -1) {
+    e.respondWith(
+      caches.open(CACHE).then(function(c) {
+        return c.match(e.request).then(function(r) {
+          return r || fetch(e.request).then(function(nr) {
+            c.put(e.request, nr.clone());
+            return nr;
+          });
+        });
+      })
+    );
+    return;
+  }
+  // For everything else, try cache first then network
+  e.respondWith(
+    caches.match(e.request).then(function(r) { return r || fetch(e.request); })
+  );
+});
